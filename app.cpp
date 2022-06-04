d7fef0202f52d375d20c0c4853169139f963ee96
Taki <javajavawookjavawook@gmail.com>
2022-06-04 16:48:47 +0900

    Add: etrobocon2021リポジトリから必要そうなファイルをコピー

---
+++ app.cpp
@@ -0,0 +1,7 @@
+#include "app.h"
+
+void main_task(intptr_t unused)
+{
+  ext_tsk();
+}
+// end::main_task_2[]
---
+++ make.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# etrobocon2021ディレクトリ下でもmakeできるようにする
+
+cd $ETROBO_ROOT
+make app=etrobocon2021 $@
