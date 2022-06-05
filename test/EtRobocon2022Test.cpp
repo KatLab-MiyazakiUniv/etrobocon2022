ef5c1d0aac578a617b73f536c1ec741f1181f0ca
Taki <javajavawookjavawook@gmail.com>
2022-06-06 08:39:57 +0900

    add: 空のEtRobocon2022クラスを追加

---
+++ test/EtRobocon2022Test.cpp
@@ -0,0 +1,17 @@
+#include "../module/EtRobocon2022.h" // このヘッダファイルのcppファイルをテスト
+#include <gtest/gtest.h>
+
+namespace etrobocon2022_test
+{
+
+    class EtRobocon2022Test : public ::testing::Test
+    {
+        friend class EtRobocon2022;
+
+    protected:
+        virtual void SetUp() {}
+        EtRobocon2022 et;
+    };
+
+    TEST_F(EtRobocon2022Test, startTest) {}
+} // namespace etrobocon2022_test
