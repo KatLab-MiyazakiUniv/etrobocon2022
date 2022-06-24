aa33fc3bc7fe586a596f8fa7b64d237ecf59493a
sugaken0528 <susunsun0528@gmail.com>
2022-06-24 10:44:30 +0900

    旋回動確認用のコードを追加

--- module/EtRobocon2022.cpp
+++ module/EtRobocon2022.cpp
@@ -5,8 +5,21 @@
  */
 
 #include "EtRobocon2022.h"
+#include "Controller.h"
+
 void EtRobocon2022::start()
 {
-  StraightRunner straight;
-  straight.runStraightToDistance(1000, 30);
+  Rotation rotation;
+  Controller controller;
+  rotation.rotateLeft(90, 50);
+  controller.sleep(100);
+  rotation.rotateRight(90, 50);
+  controller.sleep(100);
+  rotation.turnForwardRightPivot(90, 50);
+  controller.sleep(100);
+  rotation.turnBackRightPivot(90, 50);
+  controller.sleep(100);
+  rotation.turnForwardLeftPivot(90, 50);
+  controller.sleep(100);
+  rotation.turnBackLeftPivot(90, 50);
 }
