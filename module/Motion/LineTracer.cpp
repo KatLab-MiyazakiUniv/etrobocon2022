bfe5e7aefabd835b7c9df6fbc606484c84852f76
mutotaka0426 <hm17058@student.miyazaki-u.ac.jp>
2022-06-29 05:47:52 +0900

    refactor: コメントの修正

--- module/Motion/LineTracer.cpp
+++ module/Motion/LineTracer.cpp
@@ -69,7 +69,7 @@ void LineTracer::runToColor(COLOR targetColor, int targetBrightness, int pwm, co
   //左右で符号を変える
   sign = (isLeftEdge) ? -1 : 1;
 
-  //指定された色を回連続で取得するまで繰り返す
+  //指定された色をJUDGE_COUNT回連続で取得するまで繰り返す
   while(colorCount < JUDGE_COUNT) {
     currentColor = ColorJudge::getColor(measurer.getRawColor());
     if(currentColor == targetColor) {
