55bc0210b9f35ee95fc422f0bbd7f5bfb92038a6
sap2368 <92567209+sap2368@users.noreply.github.com>
2022-07-11 04:41:24 +0900

    refactor:コメントの修正

--- module/common/Logging.h
+++ module/common/Logging.h
@@ -13,27 +13,27 @@ class Logging {
   Logging();
 
   /**
-  ** 入力された文字列をLogとしてターミナルに表示
-  ** @param logMessage
-  **/
+   * 入力された文字列をLogとしてターミナルに表示
+   * @param logMessage 表示するLogメッセージ
+   */
   void writeLogMessage(const char* logMessage);
 
   /**
-  ** 入力された文字列をwarningMessageとして色を変更し、ターミナルに表示
-  ** @param warningMessage
-  **/
+   * 入力された文字列をwarningMessageとして色を変更し、ターミナルに表示
+   * @param warningMessage 表示するwarningメッセージ
+   */
   void writeWarningMessage(const char* warningMessage);
 
-  /**
-  ** 入力された文字列をerrorMessageとして色を変更し、ターミナルに表示
-  ** @param errorMessage
-  **/
+  /*
+   * 入力された文字列をerrorMessageとして色を変更し、ターミナルに表示
+   * @param errorMessage 表示するerroメッセージ
+   */
   void writeErrorMessage(const char* errorMessage);
 
   /**
-  ** 入力された文字列をhighlightLogとして色を変更し、ターミナルに表示
-  ** @param hilightLog
-  **/
+   * 入力された文字列をhighlightLogとして色を変更し、ターミナルに表示
+   * @param hilightLog 表示するhighlightlogメッセージ
+   */
   void writeHighlightLog(const char* highlightLog);
 
  private:
