f517bfc5f4e0506fee083f3384c0716d7a00bb27
Taki <javajavawookjavawook@gmail.com>
2022-07-10 20:28:38 +0900

    update: 色識別パラメータ再調整

--- module/Motion/ColorJudge.h
+++ module/Motion/ColorJudge.h
@@ -36,12 +36,12 @@ class ColorJudge {
   static COLOR getColor(rgb_raw_t const& rgb);
 
  private:
-  static constexpr int SATURATION_BORDER = 35;    // 無彩色かどうかの彩度の境界
+  static constexpr int SATURATION_BORDER = 27;    // 無彩色かどうかの彩度の境界
   static constexpr int BLACK_LIMIT_BORDER = 10;   // 黒の明度の境界
   static constexpr int WHITE_LIMIT_BORDER = 250;  // 白の明度の境界
   static constexpr int BLACK_BORDER = 110;        // 無彩色の黒の明度の境界
   static constexpr int RED_BORDER = 25;           // 赤の色相の境界
-  static constexpr int YELLOW_BORDER = 75;        // 黄の色相の境界
+  static constexpr int YELLOW_BORDER = 40;        // 黄の色相の境界
   static constexpr int GREEN_BORDER = 170;        // 緑の色相の境界
   static constexpr int BLUE_BORDER = 300;         // 青の色相の境界
   static constexpr rgb_raw_t MAX_RGB = { 244, 245, 252 };  //コースが白の時（最大）のRGB値
