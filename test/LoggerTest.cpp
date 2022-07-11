646c2bf0c340eb0ebb89e215b2339d452d0ecfba
sap2368 <92567209+sap2368@users.noreply.github.com>
2022-07-12 01:20:07 +0900

    refactor:clang-formatの修正

--- test/LoggerTest.cpp
+++ test/LoggerTest.cpp
@@ -9,26 +9,27 @@
 #include <gtest/internal/gtest-port.h>
 
 namespace etrobocon2022_test {
-TEST(LoggerTest, writeLogs)
+  TEST(LoggerTest, writeLogs)
   {
     Logger logger;
     std::string logMsg = "logs test.";
     std::string expected = logMsg + "\n";
-    testing::internal::CaptureStdout(); // 標準出力キャプチャ開始
+    testing::internal::CaptureStdout();  // 標準出力キャプチャ開始
     logger.log(logMsg.c_str());
-    std::string actual = testing::internal::GetCapturedStdout(); // キャプチャ終了
+    std::string actual = testing::internal::GetCapturedStdout();  // キャプチャ終了
     ASSERT_STREQ(expected.c_str(), actual.c_str());
   }
+
   TEST(LoggingTest, writeWarningMessage)
   {
     Logger logger;
     std::string logMsg = "WarningMessage test.";
     std::string expected = "\x1b[36m";  // 文字色をシアンに
     expected += "Warning: " + logMsg + "\n";
-    expected += "\x1b[39m";  // 文字色をデフォルトに戻す
-    testing::internal::CaptureStdout(); // 標準出力キャプチャ開始
+    expected += "\x1b[39m";              // 文字色をデフォルトに戻す
+    testing::internal::CaptureStdout();  // 標準出力キャプチャ開始
     logger.logWarning(logMsg.c_str());
-    std::string actual = testing::internal::GetCapturedStdout(); // キャプチャ終了
+    std::string actual = testing::internal::GetCapturedStdout();  // キャプチャ終了
     ASSERT_STREQ(expected.c_str(), actual.c_str());
   }
 
@@ -38,10 +39,10 @@ TEST(LoggerTest, writeLogs)
     std::string logMsg = "ErrorMessage test.";
     std::string expected = "\x1b[35m";  // 文字色をマゼンタに
     expected += "Error: " + logMsg + "\n";
-    expected += "\x1b[39m";  // 文字色をデフォルトに戻す
-    testing::internal::CaptureStdout(); // 標準出力キャプチャ開始
+    expected += "\x1b[39m";              // 文字色をデフォルトに戻す
+    testing::internal::CaptureStdout();  // 標準出力キャプチャ開始
     logger.logError(logMsg.c_str());
-    std::string actual = testing::internal::GetCapturedStdout(); // キャプチャ終了
+    std::string actual = testing::internal::GetCapturedStdout();  // キャプチャ終了
     ASSERT_STREQ(expected.c_str(), actual.c_str());
   }
 
@@ -50,11 +51,11 @@ TEST(LoggerTest, writeLogs)
     Logger logger;
     std::string logMsg = "HighlightLog test.";
     std::string expected = "\x1b[32m";  // 文字色を緑色に
-    expected +=  logMsg + "\n";
-    expected += "\x1b[39m";  // 文字色をデフォルトに戻す
-    testing::internal::CaptureStdout(); // 標準出力キャプチャ開始
+    expected += logMsg + "\n";
+    expected += "\x1b[39m";              // 文字色をデフォルトに戻す
+    testing::internal::CaptureStdout();  // 標準出力キャプチャ開始
     logger.logHighlight(logMsg.c_str());
-    std::string actual = testing::internal::GetCapturedStdout(); // キャプチャ終了
+    std::string actual = testing::internal::GetCapturedStdout();  // キャプチャ終了
     ASSERT_STREQ(expected.c_str(), actual.c_str());
   }
 
