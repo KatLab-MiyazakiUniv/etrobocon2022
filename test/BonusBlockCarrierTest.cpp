5c810072d61a706d2656bb2fafac7191f527abbd
sugaken0528 <susunsun0528@gmail.com>
2022-07-14 14:46:42 +0900

    update : 余分なインスタンス化の記述を削除

--- test/BonusBlockCarrierTest.cpp
+++ test/BonusBlockCarrierTest.cpp
@@ -5,10 +5,6 @@
  */
 
 #include "BonusBlockCarrier.h"
-#include "Rotation.h"
-#include "StraightRunner.h"
-#include "LineTracer.h"
-#include "Controller.h"
 #include <gtest/gtest.h>
 
 namespace etrobocon2022_test {
@@ -16,10 +12,6 @@ namespace etrobocon2022_test {
   // 呼び出すだけのテスト
   TEST(BonusBlockCarrierTest, run)
   {
-    Rotation rotation;
-    StraightRunner straightRunner;
-    LineTracer lineTracer(true);
-    Controller controller;
     int targetBrightness = 45;
 
     // ボーナスブロック運搬を実行
