6385a1ea906fe88bb9eead95d579660fdfab840a
sap2368 <92567209+sap2368@users.noreply.github.com>
2022-07-14 22:06:34 +0900

    add:ColorBlockCarrierクラスの追加

---
+++ module/Motion/ColorBlockCarrier.h
@@ -0,0 +1,26 @@
+/**
+ * @file ColorBlockCarrier.h
+ * @brief カラーブロックをベースエリアまで運搬する
+ * @author sap2368
+ */
+
+#ifndef COLOR_BLOCK_CARRIER_H
+#define COLOR_BLOCK_CARRIER_H
+
+#include "Rotation.h"
+#include "LineTracer.h"
+#include "StraightRunner.h"
+#include "Controller.h"
+
+class ColorBlockCarrier {
+ public:
+  /**
+   * ベースエリアにボーナスブロックを運ぶ
+   * @param targetBrightness 目標輝度
+   */
+  static void run(int targetBrightness);
+
+ private:
+  ColorBlockCarrier();  // インスタンス化を禁止する
+};
+#endif
