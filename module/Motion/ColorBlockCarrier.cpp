d5ff05a44c3c966abfb67bcd9539787675061d48
sap2368 <92567209+sap2368@users.noreply.github.com>
2022-07-19 03:53:35 +0900

    update:ColorBlockCarrierの調整

--- module/Motion/ColorBlockCarrier.cpp
+++ module/Motion/ColorBlockCarrier.cpp
@@ -16,41 +16,41 @@ void ColorBlockCarrier::run(int targetBrightness)
   Controller controller;
 
   // 交点内を直進
-  straightRunner.run(10, -50);
+  straightRunner.run(40, -50);
 
   // 緑を認識するまでライントレース
   lineTracer.runToColor(COLOR::GREEN, targetBrightness, -40, PidGain(0.1, 0.08, 0.08));
 
-  // // 右に45度回頭
-  // rotation.rotateRight(45, 70);
+  // 右に38度回頭
+  rotation.rotateRight(38, 70);
 
-  // // ブロックまで直進
-  // straightRunner.run(34, 50);
+  // ブロックまで直進
+  straightRunner.run(145, 60);
 
-  // // 右に45度回頭
-  // rotation.rotateRight(45, 70);
+  // 右に52度回頭
+  rotation.rotateRight(52, 70);
 
-  // // 黒線まで直進
-  // straightRunner.run(24, 50);
+  // 黒線まで直進
+  straightRunner.run(152, 60);
 
-  // // 右に90度回頭
-  // rotation.rotateRight(90, 70);
+  // 右に90度回頭
+  rotation.rotateRight(90, 70);
 
-  // // 黄色を認識するまでライントレース
-  // lineTracer.runToColor(COLOR::YELLOW, targetBrightness, 40, PidGain(0.1, 0.08, 0.08));
+  // 直進を安定させるために1秒待機
+  controller.sleep(1000000);
 
-  // // 交点内を直進
-  // straightRunner.run(5, 50);
+  // 黄色を認識するまでライントレース
+  lineTracer.runToColor(COLOR::YELLOW, targetBrightness, 50, PidGain(0.1, 0.08, 0.08));
+  // 交点内を直進
+  straightRunner.run(35, 70);
 
-  // // 赤を認識するまでライントレース
-  // lineTracer.runToColor(COLOR::RED, targetBrightness, 40, PidGain(0.1, 0.08, 0.08));
+  // 赤を認識するまでライントレース
+  lineTracer.runToColor(COLOR::RED, targetBrightness, 50, PidGain(0.1, 0.08, 0.08));
+  // 交点内を直進
+  straightRunner.run(80, 70);
 
-  // // 交点内を直進
-  // straightRunner.run(5, 50);
-
-  // // 赤を認識するまでライントレース
-  // lineTracer.runToColor(COLOR::RED, targetBrightness, 40, PidGain(0.1, 0.08, 0.08));
-
-  // // 交点内を直進
-  // straightRunner.run(5, 50);
+  // 赤を認識するまでライントレース
+  lineTracer.runToColor(COLOR::RED, targetBrightness, 50, PidGain(0.1, 0.08, 0.08));
+  // 交点内を直進
+  straightRunner.run(35, 70);
 }
