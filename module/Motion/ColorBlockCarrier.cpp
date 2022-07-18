b7be46811140192753f855d9f36e411b4d3dc368
sap2368 <92567209+sap2368@users.noreply.github.com>
2022-07-19 02:23:23 +0900

    test:No42

--- module/Motion/ColorBlockCarrier.cpp
+++ module/Motion/ColorBlockCarrier.cpp
@@ -32,7 +32,7 @@ void ColorBlockCarrier::run(int targetBrightness)
   rotation.rotateRight(52, 70);
 
   // 黒線まで直進
-  straightRunner.run(155, 60);
+  straightRunner.run(150, 60);
 
   // 右に90度回頭
   rotation.rotateRight(90, 70);
