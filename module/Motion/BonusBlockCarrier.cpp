d277dccb2d325a5821215081f0fa5bc450ef349b
kawano <hi18014@student.miyazaki-u.ac.jp>
2022-07-19 21:30:34 +0900

    refactor:#56コメントを修正

--- module/Motion/BonusBlockCarrier.cpp
+++ module/Motion/BonusBlockCarrier.cpp
@@ -21,11 +21,8 @@ void BonusBlockCarrier::run(int targetBrightness)
   // エッジ変更 true:左エッジ, false:右エッジ
   lineTracer.setIsLeftEdge(false);
 
-  // 赤を認識するまでライントレース
+  // 赤(端点サークル)を認識するまでライントレース
   lineTracer.runToColor(COLOR::RED, targetBrightness, 40, PidGain(0.1, 0.08, 0.08));
-
-  // エッジ変更 true:左エッジ, false:右エッジ
-  // lineTracer.setIsLeftEdge(true);
 
   // 左に90度ピボットターン
   rotation.turnForwardLeftPivot(89, 85);
@@ -33,34 +30,31 @@ void BonusBlockCarrier::run(int targetBrightness)
   // 直進を安定させるために1秒待機
   controller.sleep(1000000);
 
-  // 指定した色まで直進
+  // 緑の交点マーカー(右下)まで直進
   straightRunner.runStraightToColor(50, COLOR::GREEN);
 
-  // 指定した距離(交点マーカーを通り過ぎる距離)を直進
+  // 直進(交点マーカーを通り過ぎる)
   straightRunner.run(80, 50);
 
-  // ブロックがひっかかるため、引っかかるところの前まで直進
-  lineTracer.run(100, targetBrightness, 50, PidGain(0.5, 0.08, 0.08));
+  // 緑の交点マーカー(左下)までライントレース
+  lineTracer.runToColor(COLOR::GREEN, targetBrightness, 50, PidGain(0.5, 0.08, 0.08));
 
-  // // 指定した色までライントレース
-  // lineTracer.runToColor(COLOR::GREEN, targetBrightness, 50, PidGain(0.5, 0.08, 0.08));
+  // 直進(交点マーカーを通り過ぎる)
+  straightRunner.run(80, 50);
 
-  // // 指定した距離(交点マーカーを通り過ぎる距離)を直進
-  // straightRunner.run(80, 50);
+  /**
+   * そのまま90度回頭して設置するとブロックがベースエリアからはみ出る場合があるため，
+   * 回頭と後退を分けて実行する
+   */
+  // 左に80度回頭
+  rotation.rotateLeft(80, 70);
 
-  // /**
-  //  * そのまま90度回頭して設置するとブロックがベースエリアからはみ出る場合があるため，
-  //  * 回頭と後退を分けて実行する
-  //  */
-  // // 左に80度回頭
-  // rotation.rotateLeft(80, 70);
+  // 後退
+  straightRunner.run(20, -55);
 
-  // // 後退
-  // straightRunner.run(20, -55);
+  // 左に10度回頭
+  rotation.rotateLeft(10, 80);
 
-  // // 左に10度回頭
-  // rotation.rotateLeft(10, 80);
-
-  // // 後退
-  // straightRunner.run(40, -55);
+  // 後退
+  straightRunner.run(40, -55);
 }
