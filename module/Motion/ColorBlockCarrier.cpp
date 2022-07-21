3afab6730a7395fcd3e6f85794d3a0feca407660
sap2368 <92567209+sap2368@users.noreply.github.com>
2022-07-21 17:34:50 +0900

    3

--- module/Motion/ColorBlockCarrier.cpp
+++ module/Motion/ColorBlockCarrier.cpp
@@ -21,19 +21,19 @@ void ColorBlockCarrier::run(int targetBrightness)
   lineTracer.runToColor(COLOR::GREEN, targetBrightness, -40, PidGain(0.1, 0.08, 0.08));
 
   // 右に38度回頭
-  rotation.rotateRight(38, 70);
+  rotation.rotateRight(38, 50);
 
   // ブロックまで直進
-  straightRunner.run(145, 60);
+  straightRunner.run(150, 50);
 
   // 右に52度回頭
-  rotation.rotateRight(52, 70);
+  rotation.rotateRight(52, 60);
 
   // 黒線まで直進
-  straightRunner.run(152, 60);
+  straightRunner.run(135, 60);
 
   // 右に90度回頭
-  rotation.rotateRight(90, 70);
+  rotation.rotateRight(90, 60);
 
   // 直進を安定させるために1秒待機
   controller.sleep(1000000);
