09f77b936b110ffc4d6061a2df21aeb975514405
sap2368 <92567209+sap2368@users.noreply.github.com>
2022-07-21 17:17:24 +0900

    test:1

--- module/Motion/ColorBlockCarrier.cpp
+++ module/Motion/ColorBlockCarrier.cpp
@@ -18,41 +18,41 @@ void ColorBlockCarrier::run(int targetBrightness)
   // straightRunner.run(40, -50);
 
   // 緑を認識するまでライントレース
-  lineTracer.runToColor(COLOR::YELLOW, targetBrightness, -40, PidGain(0.1, 0.08, 0.08));
+  lineTracer.runToColor(COLOR::GREEN, targetBrightness, -40, PidGain(0.1, 0.08, 0.08));
 
-  // // 右に38度回頭
-  // rotation.rotateRight(38, 70);
+  // 右に38度回頭
+  rotation.rotateRight(38, 70);
 
-  // // ブロックまで直進
-  // straightRunner.run(145, 60);
+  // ブロックまで直進
+  straightRunner.run(145, 60);
 
-  // // 右に52度回頭
-  // rotation.rotateRight(52, 70);
+  // 右に52度回頭
+  rotation.rotateRight(52, 70);
 
-  // // 黒線まで直進
-  // straightRunner.run(152, 60);
+  // 黒線まで直進
+  straightRunner.run(152, 60);
 
-  // // 右に90度回頭
-  // rotation.rotateRight(90, 70);
+  // 右に90度回頭
+  rotation.rotateRight(90, 70);
 
-  // // 直進を安定させるために1秒待機
-  // controller.sleep(1000000);
+  // 直進を安定させるために1秒待機
+  controller.sleep(1000000);
 
-  // // 黄色を認識するまでライントレース
-  // lineTracer.runToColor(COLOR::YELLOW, targetBrightness, 50, PidGain(0.1, 0.08, 0.08));
+  // 黄色を認識するまでライントレース
+  lineTracer.runToColor(COLOR::YELLOW, targetBrightness, 50, PidGain(0.1, 0.08, 0.08));
 
-  // // 交点内を直進
-  // straightRunner.run(35, 70);
+  // 交点内を直進
+  straightRunner.run(35, 70);
 
-  // // 赤を認識するまでライントレース
-  // lineTracer.runToColor(COLOR::RED, targetBrightness, 50, PidGain(0.1, 0.08, 0.08));
+  // 赤を認識するまでライントレース
+  lineTracer.runToColor(COLOR::RED, targetBrightness, 50, PidGain(0.1, 0.08, 0.08));
 
-  // // 交点内を直進
-  // straightRunner.run(80, 70);
+  // 交点内を直進
+  straightRunner.run(80, 70);
 
-  // // 赤を認識するまでライントレース
-  // lineTracer.runToColor(COLOR::RED, targetBrightness, 50, PidGain(0.1, 0.08, 0.08));
+  // 赤を認識するまでライントレース
+  lineTracer.runToColor(COLOR::RED, targetBrightness, 50, PidGain(0.1, 0.08, 0.08));
 
-  // // 交点内を直進
-  // straightRunner.run(35, 70);
+  // 交点内を直進
+  straightRunner.run(35, 70);
 }
