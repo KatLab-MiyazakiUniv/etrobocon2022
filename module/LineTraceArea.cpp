4c15e4d005f351e9f77088a69b03889ee75f039b
sugaken0528 <susunsun0528@gmail.com>
2022-08-02 16:06:35 +0900

    update : ライントレースエリアの調整

--- module/LineTraceArea.cpp
+++ module/LineTraceArea.cpp
@@ -1,7 +1,7 @@
 /**
  * @file LineTraceArea.cpp
  * @brief ライントレースエリアを攻略するクラス
- * @author mutotaka0426 kawanoichi
+ * @author mutotaka0426 kawanoichi sap2368
  */
 
 #include "LineTraceArea.h"
@@ -15,11 +15,11 @@ const array<double, LineTraceArea::RIGHT_SECTION_SIZE> LineTraceArea::RIGHT_SECT
 
 // Lコースのパラメータを初期化する（調整距離, PWM値, PIDゲイン）
 const array<SectionParam, LineTraceArea::LEFT_SECTION_SIZE> LineTraceArea::LEFT_COURSE_INFO = {
-  SectionParam{ 30, 50, PidGain(0.2, 0.8, 0.1) },     //直進
-  SectionParam{ 100, 30, PidGain(1.0, 0.7, 0.2) },    //カーブ
-  SectionParam{ -100, 50, PidGain(0.3, 0.8, 0.08) },  //直進
-  SectionParam{ 160, 30, PidGain(1.0, 0.9, 1.0) },    //カーブ
-  SectionParam{ -10, 50, PidGain(0.3, 0.7, 0.08) }    //直進
+  SectionParam{ 10, 70, PidGain(0.2, 0.8, 0.1) },     //直進
+  SectionParam{ 160, 40, PidGain(1.0, 0.9, 1.0) },    //カーブ
+  SectionParam{ -160, 70, PidGain(0.3, 0.8, 0.08) },  //直進
+  SectionParam{ 250, 40, PidGain(1.0, 0.9, 1.0) },    //カーブ
+  SectionParam{ 15, 70, PidGain(0.3, 0.7, 0.08) }     //直進
 };
 
 // Rコースのパラメータを初期化する（調整距離, PWM値, PIDゲイン）
@@ -52,7 +52,6 @@ void LineTraceArea::runLineTraceArea(const bool isLeftCourse, bool& isLeftEdge,
 
   // LineTracerにエッジを与えてインスタンス化する
   LineTracer lineTracer(isLeftEdge);
-
   const char* course = isLeftCourse ? "Left" : "Right";
   sprintf(buf, "\nRun on the %s Course\n", course);
   logger.logHighlight(buf);
