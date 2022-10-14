e7cf21b485827f4588ef180480ab959953d3ac84
mutotaka0426 <hm17058@student.miyazaki-u.ac.jp>
2022-10-15 05:12:24 +0900

    update: ログファイルをファイル出力するように拡張

--- module/common/Logger.cpp
+++ module/common/Logger.cpp
@@ -1,7 +1,7 @@
 /**
  * @file Logger.cpp
  * @brief 動作確認に用いるprintf()関数を所持するクラス
- * @author sap2368
+ * @author sap2368 mutotaka0426
  */
 #include "Logger.h"
 
@@ -9,30 +9,73 @@ Logger::Logger() {}
 
 void Logger::log(const char* logMessage)
 {
-  const char* log = logMessage;
-  printf("%s\n", log);
+  const int BUF_SIZE = 128;
+  char message[BUF_SIZE];  // 表示するメッセージ
+  snprintf(message, BUF_SIZE, "%s\n", logMessage);
+  printf("%s", message);
+
+  strncat(logs, message, sizeof(logs));  // logsとmessageを結合する
 }
 
 void Logger::logWarning(const char* warningMessage)
 {
-  const char* warning = warningMessage;
+  const int BUF_SIZE = 128;
+  char message[BUF_SIZE];  // 表示するメッセージ
+  snprintf(message, BUF_SIZE, "%s\n", warningMessage);
   printf("\x1b[36m"); /* 文字色をシアンに */
-  printf("Warning: %s\n", warning);
+  printf("Warning: %s", message);
   printf("\x1b[39m"); /* 文字色をデフォルトに戻す */
+
+  strncat(logs, message, sizeof(logs));  // logsとmessageを結合する
 }
 
 void Logger::logError(const char* errorMessage)
 {
-  const char* error = errorMessage;
+  const int BUF_SIZE = 128;
+  char message[BUF_SIZE];  // 表示するメッセージ
+  snprintf(message, BUF_SIZE, "%s\n", errorMessage);
   printf("\x1b[35m"); /* 文字色をマゼンタに */
-  printf("Error: %s\n", error);
+  printf("Error: %s", message);
   printf("\x1b[39m"); /* 文字色をデフォルトに戻す */
+
+  strncat(logs, message, sizeof(logs));  // logsとmessageを結合する
 }
 
 void Logger::logHighlight(const char* highlightLog)
 {
-  const char* hlog = highlightLog;
+  const int BUF_SIZE = 128;
+  char message[BUF_SIZE];  // 表示するメッセージ
+  snprintf(message, BUF_SIZE, "%s\n", highlightLog);
   printf("\x1b[32m"); /* 文字色を緑色に */
-  printf("%s\n", hlog);
+  printf("%s", message);
   printf("\x1b[39m"); /* 文字色をデフォルトに戻す */
+
+  strncat(logs, message, sizeof(logs));  // logsとmessageを結合する
 }
+
+void Logger::outputToFile()
+{
+  FILE* outputFile;
+  const char* fileName = "logfile.txt";  // 暫定のファイル名
+  outputFile = fopen(fileName, "w");     // logfile.txtを作成
+  if(outputFile == NULL) {
+    logWarning("cannot open file");
+    return;
+  }
+  fprintf(outputFile, logs);  // logsの内容をlogfile.txtに書き込む
+  fclose(outputFile);
+
+  /*
+  NOTE: ロボコン環境だと，時刻を取得するtime.hのtime()や，
+  ディレクトリを生成するためのヘッダdirect.hを使用できないため（多分），
+  bashでlogfile.txtの名前をログ生成時刻に変更し，logfiles/に移動する
+  */
+  system("bash ./etrobocon2022/scripts/organizeLogfile.sh &");
+}
+
+void Logger::initLogs()
+{
+  logs[0] = '\0';  // 文字列を初期化
+}
+
+char Logger::logs[65536] = "";  // logsを初期化
