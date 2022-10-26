209644150e1eb36e1e5b41e741e729299bb070c9
Takahiro55555 <31930148+Takahiro55555@users.noreply.github.com>
2022-10-26 21:49:25 +0900

    update: 個人開発環境での動作を確認

--- rear_camera_py/rear_camera/camera_interface.py
+++ rear_camera_py/rear_camera/camera_interface.py
@@ -1,15 +1,26 @@
 from typing import Union
-import cv2
-import numpy as np
 
+import cv2
+from picamera2 import Picamera2
+import numpy as np
 
 class CameraInterface:
     def __init__(self, camera_id: int = 0) -> None:
         self.__camera_id = camera_id
+        self.__picam2 = None
+
+    @property
+    def picam2(self) -> None:
+        pass
+
+    @picam2.getter
+    def picam2(self) -> Picamera2:
+        if self.__picam2 is None:
+            picam2 = Picamera2(camera_num=self.__camera_id)
+            picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (1640, 1232)}))
+            picam2.start()
+            self.__picam2 = picam2
+        return self.__picam2
 
     def capture_image(self) -> Union[np.ndarray, None]:
-        import subprocess
-        fname = 'input.png'
-        cmd = ["/usr/bin/libcamera-still", "-n", "--encoding",  "png", "--width", "3280", "--height", "2464", "-o", fname]
-        res = subprocess.call(cmd)
-        return cv2.imread(fname)
+        return self.picam2.capture_array()
