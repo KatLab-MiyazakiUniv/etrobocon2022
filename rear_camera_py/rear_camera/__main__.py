b1fe21ad37a3309930796d187730b72658047ac1
Takahiro55555 <31930148+Takahiro55555@users.noreply.github.com>
2022-10-26 22:35:41 +0900

    update: カメラIDをオプションとして指定できるようにした

--- rear_camera_py/rear_camera/__main__.py
+++ rear_camera_py/rear_camera/__main__.py
@@ -1,18 +1,25 @@
 import argparse
 
 from rear_camera.calibrator import Calibrator
+from rear_camera.camera_interface import CameraInterface
 from rear_camera.line_angle_calculator import LineAngleCalculator
 
 parser = argparse.ArgumentParser(description="回頭補正用角度算出プログラム")
+parser.add_argument("--camera-num", type=int, default=0,
+                    help="カメラID")
 parser.add_argument("--calibrate", action="store_true",
                     help="キャリブレーションを行う場合指定するフラグ")
+parser.add_argument("--debug", action="store_true",
+                    help="デバッグを行う場合指定するフラグ")
 args = parser.parse_args()
 
 
+camera = CameraInterface(args.camera_num)
+
 if args.calibrate:
-    calibrator = Calibrator()
+    calibrator = Calibrator(camera)
     calibrator.calibrate()
     print("Calibrated")
 else:
-    lac = LineAngleCalculator(debug=True)
+    lac = LineAngleCalculator(camera, debug=args.debug)
     print(lac.calc_yaw_angle())
