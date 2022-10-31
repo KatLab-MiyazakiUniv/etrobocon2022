7fd848af00edda6b246555d4729cf943fd8468b6
kodama0720 <bnuy1221@gmail.com>
2022-10-31 14:46:03 +0900

    update:staticmethodに変更、細かい修正

--- rear_camera_py/rear_camera/black_extractor.py
+++ rear_camera_py/rear_camera/black_extractor.py
@@ -6,13 +6,13 @@
 import cv2
 from typing import List
 import numpy as np
-import time
 
 
 class BlackExtractor:
-    """画像の黒抽出クラス."""
+    """画像の色変換クラス."""
 
-    def extract_black(self, rear_camera_img: cv2.Mat) -> List[int]:
+    @staticmethod
+    def extract_black(rear_camera_img: cv2.Mat) -> List[int]:
         """画像から黒線を抽出する関数.
 
         Args:
@@ -21,25 +21,25 @@ class BlackExtractor:
         Returns:
             2値変換した画像データ: List[int]
         """
-        lower = np.array([0, 0, 80])
-        upper = np.array([180, 245, 255])
+        MIN_WHITE_VALUE = 80  # 白と識別するための最低明度
+        lower = np.array([0, 0, MIN_WHITE_VALUE])
+        upper = np.array([180, 255, 255])
 
         # BGR色空間からHSV色空間への変換
         hsv = cv2.cvtColor(rear_camera_img, cv2.COLOR_BGR2HSV)
         # 2値変換
-        result = cv2.inRange(hsv, lower, upper)
+        binary_img = cv2.inRange(hsv, lower, upper)
 
-        return result
+        return binary_img
 
 
 if __name__ == "__main__":
     read_path = "rear_camera1.png"
     save_path = "binary_" + read_path
     rear_camera_img = cv2.imread(read_path)
-    # インスタンス化
-    black_extractor = BlackExtractor()
+
     # 2値変換
-    result = black_extractor.extract_black(rear_camera_img)
+    result = BlackExtractor.extract_black(rear_camera_img)
     # 2値画像を保存
     cv2.imwrite(save_path, result)
 
