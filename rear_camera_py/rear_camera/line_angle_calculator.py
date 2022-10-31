5a0f1bcb9dcf17ece20fa468bf2b66ffdbe5fa9b
Takahiro55555 <31930148+Takahiro55555@users.noreply.github.com>
2022-10-31 21:20:11 +0900

    refactor: コメント等を追加

--- rear_camera_py/rear_camera/line_angle_calculator.py
+++ rear_camera_py/rear_camera/line_angle_calculator.py
@@ -1,3 +1,4 @@
+"""コースの直線に対する機体の角度算出するモジュール."""
 
 import json
 import os
@@ -11,6 +12,8 @@ from rear_camera.camera_interface import CameraInterface
 
 
 class LineAngleCalculator:
+    """コースの直線に対する機体の角度算出するクラス."""
+
     def __init__(
         self,
         camera_interface: CameraInterface = CameraInterface(),
@@ -19,6 +22,19 @@ class LineAngleCalculator:
         debug: bool = False,
         debug_dir="debug"
     ) -> None:
+        """コンストラクタ.
+
+        Args:
+            camera_interface (CameraInterface, optional): リアカメラ画像を取得するためのCameraInterfaceインスタンス. Defaults to CameraInterface().
+            trans_mat_file (str, optional): 射影変換用パラメータファイル名. Defaults to "rear_camera_param.npy".
+            distance_file (str, optional): 射影変換後の画像座標と走行体の中心からの距離等の関係を保持するパラメータファイル名. Defaults to "rear_camera_distance_param.json".
+            debug (bool, optional): Trueに設定するとデバッグ用の画像ファイルが生成される. Defaults to False.
+            debug_dir (str, optional): デバッグ用の画像ファイルを生成するディレクトリ(存在しない場合は自動で生成される). Defaults to "debug".
+
+        Raises:
+            FileNotFoundError: 各種パラメータファイルが見つからない場合に発生.
+            KeyError: 射影変換後の画像座標と走行体の中心からの距離等の関係を保持するパラメータファイルのデータが一部でも欠損している場合に発生.
+        """
         self.__camera_interface = camera_interface
         if not os.path.isfile(trans_mat_file):
             raise FileNotFoundError("file name: '%s'" % trans_mat_file)
@@ -54,15 +70,23 @@ class LineAngleCalculator:
         self.__debug_dir = debug_dir
 
     def calc_yaw_angle(self, contours_area_size_min_threshold_mm2: int = 9999, debug_img_fname_prefix: str = "angle") -> Union[float, None]:
+        """検出した直線と機体の中心線とのなす角を算出する.
+
+        Args:
+            contours_area_size_min_threshold_mm2 (int, optional): ノイズとして排除する検出した直線の面積の閾値. Defaults to 9999.
+            debug_img_fname_prefix (str, optional): デバッグ用画像の接頭辞. Defaults to "angle".
+
+        Returns:
+            Union[float, None]: 検出した直線と機体の中心線とのなす角、直線を検出できなかった場合は`None`を返す.
+        """
         img = self.__camera_interface.capture_image()
         if img is None:
             return None
-        return self.__detect_lines(img, contours_area_size_min_threshold_mm2, debug_img_fname_prefix)
 
-    def __detect_lines(self, img: np.ndarray, contours_area_size_min_threshold_mm2, debug_img_fname_prefix) -> Union[float, None]:
         if self.__debug:
             debug_time = time.time()
-            img_fname = "%s_%f_captured.png" % (debug_img_fname_prefix, debug_time)
+            img_fname = "%s_%f_captured.png" % (
+                debug_img_fname_prefix, debug_time)
             debug_img_path = os.path.join(self.__debug_dir, img_fname)
             cv2.imwrite(debug_img_path, img)
         img_transformed = self.get_transformed_image(img)
@@ -99,7 +123,8 @@ class LineAngleCalculator:
         if self.__debug:
             debug_img = self.draw_detected_lines_on_the_image(
                 img_transformed, cnt, angle_2_y)
-            img_fname = "%s_%f_detected.png" % (debug_img_fname_prefix, debug_time)
+            img_fname = "%s_%f_detected.png" % (
+                debug_img_fname_prefix, debug_time)
             debug_img_path = os.path.join(self.__debug_dir, img_fname)
             cv2.imwrite(debug_img_path, debug_img)
         return float(angle_2_y)
@@ -112,33 +137,108 @@ class LineAngleCalculator:
         contour_area_pix = np.sum(tmp_img_for_calc_contour_area_mm2)
 
     def __calc_contour_score(self, contour: np.ndarray, img_h: int, img_w: int) -> float:
+        """直線と機体のなす角を算出する際に採用する直線を決定するためのスコアを算出する関数.
+        現状は、直線の中心と機体の中心間の距離を用いている.
+
+        Args:
+            contour (np.ndarray): 検出した直線の輪郭データ
+            img_h (int): 画像の縦幅[pix]
+            img_w (int): 画像の横幅[pix]
+
+        Returns:
+            float: スコア
+        """
         _, y_mm = self.calc_contour_runner_base_center_mm(
             contour, img_h, img_w)
         return y_mm
 
     @staticmethod
     def calc_contour_center_pix(contour: np.ndarray) -> Tuple[float, float]:
+        """検出した直線(の輪郭)の重心を算出する関数.
+
+        NOTE:
+            - [参考サイト](https://cvtech.cc/pycvmoment/)
+
+        Args:
+            contour (np.ndarray): 輪郭データ.
+
+        Returns:
+            Tuple[float, float]: 重心座標[pix]
+        """
         mu = cv2.moments(contour)
         x, y = mu["m10"]/mu["m00"], mu["m01"]/mu["m00"]
         return x, y
 
     def calc_contour_runner_base_center_mm(self, contour: np.ndarray, img_h: int, img_w: int) -> Tuple[float, float]:
+        """機体の中心を基準とした直線(の輪郭)の中心座標[mm]を算出する.
+
+        Args:
+            contour (np.ndarray): 検出した直線の輪郭.
+            img_h (int): 画像の縦幅[pix].
+            img_w (int): 画像の横幅[pix].
+
+        Returns:
+            Tuple[float, float]: 機体の中心を基準とした直線(の輪郭)の中心座標(x, y)[mm].
+        """
         px, py = self.calc_contour_center_pix(contour)
         x_mm, y_mm = self.image_base_coordinate_to_runner_base_coordinate_mm(
             px, py, img_h, img_w)
         return x_mm, y_mm
 
     def get_transformed_image(self, img: np.ndarray, borderValue=(255, 255, 255)) -> np.ndarray:
+        """射影変換を行った画像を取得する.
+
+        Args:
+            img (np.ndarray): 射影変換前の画像.
+            borderValue (tuple, optional): 射影変換後の画像における余白部分の色. Defaults to (255, 255, 255).
+
+        Returns:
+            np.ndarray: 射影変換後の画像.
+        """
         return cv2.warpPerspective(
             img, self.__trans_mat, (img.shape[1], img.shape[0]), borderValue=borderValue)
 
     def pix_to_mm(self, pix: Union[int, float]) -> float:
+        """pixをmmに変換する.
+
+        Args:
+            pix (Union[int, float]): 変換したい値[pix]
+
+        Returns:
+            float: 変換結果[mm]
+        """
         return pix * 52.5 / self.__distance_from_center_52_5mm
 
     def mm_to_pix(self, mm: Union[int, float]) -> float:
+        """mmをpixに変換する
+
+        Args:
+            mm (Union[int, float]): 変換したい値[mm]
+
+        Returns:
+            float: 変換結果[mm]
+        """
         return mm * self.__distance_from_center_52_5mm / 52.5
 
-    def image_base_coordinate_to_runner_base_coordinate_mm(self, img_x: Union[int, float], img_y: Union[int, float], img_h: int, img_w: int) -> Tuple[float, float]:
+    def image_base_coordinate_to_runner_base_coordinate_mm(
+        self,
+        img_x: Union[int, float],
+        img_y: Union[int, float],
+        img_h: int,
+        img_w: int
+    ) -> Tuple[float, float]:
+        """画像上の座標を機体の中心を原点とした座標に変換する.
+
+        Args:
+            img_x (Union[int, float]): 画像上のX座標[pix].
+            img_y (Union[int, float]): 画像上のY座標[pix].
+            img_h (int): 画像の縦幅[pix].
+            img_w (int): 画像の横幅[pix].
+
+        Returns:
+            Tuple[float, float]: 変換後の座標(x, y)[mm].
+        """
+
         distance_between_aruco_markers_center_and_runner_center_mm = 312.5
         img_x_axis_and_runner_x_axis_distance_mm = (
             self.pix_to_mm(self.__height_offset_from_center) + distance_between_aruco_markers_center_and_runner_center_mm)
@@ -149,23 +249,40 @@ class LineAngleCalculator:
             (img_h_mm - self.pix_to_mm(img_y))
         return runner_base_x_mm, runner_base_y_mm
 
-    def runner_base_coordinate_to_image_base_coordinate_pix(self, rx: Union[int, float], ry: Union[int, float], img_h: int, img_w: int) -> Tuple[int, int]:
+    def runner_base_coordinate_to_image_base_coordinate_pix(
+        self,
+        rx: Union[int, float],
+        ry: Union[int, float],
+        img_h: int,
+        img_w: int
+    ) -> Tuple[int, int]:
+        """機体の中心を原点とした座標に変換する.
+
+        Args:
+            rx (Union[int, float]): 走行体の中心を原点としたX座標[mm].
+            ry (Union[int, float]): 走行体の中心を原点としたY座標[mm].
+            img_h (int): 画像の縦幅[pix].
+            img_w (int): 画像の横幅[pix].
+
+        Returns:
+            Tuple[int, int]: 変換後の座標(x, y)[pix].
+        """
         pix_x = self.mm_to_pix(rx) + img_w/2
         pix_y = self.__height_offset_from_center + 312.5 - self.mm_to_pix(ry)
         return pix_x, pix_y
 
-    def __calc_runner_base_contour_moment_mm(self, contour: np.ndarray, img_h: int, img_w) -> Tuple[float, float]:
-        moment_x, moment_y = self.__calc_image_base_contour_moment_pix(contour)
-        return self.image_base_coordinate_to_runner_base_coordinate_mm(moment_x, moment_y, img_h, img_w)
-
-    def __calc_image_base_contour_moment_pix(self, contour: np.ndarray) -> Tuple[float, float]:
-        moment = cv2.moments(contour)
-        cx = moment['m10']/moment['m00']
-        cy = moment['m01']/moment['m00']
-        return cx, cy
-
     def draw_detected_lines_on_the_image(self, img: np.ndarray, contour: np.ndarray, angle: float) -> np.ndarray:
-        h, w = img.shape[:2]
+        """検出した直線及び、その直線と機体の中心線のなす角を直線の検出用いた画像へ描画するデバッグ用関数.
+
+        Args:
+            img (np.ndarray): 直線の検出に用いた画像データ.
+            contour (np.ndarray): 検出した直線の輪郭データ.
+            angle (float): 検出した直線と機体の中心線とのなす角.
+
+        Returns:
+            np.ndarray: 検出した直線及び、その直線と機体の中心線のなす角を描画した画像データ.
+        """
+        _, w = img.shape[:2]
         vx, vy, x, y = cv2.fitLine(contour, cv2.DIST_L2, 0, 0.01, 0.01)
         lefty = int((-x*vy/vx)+y)
         righty = int(((w-x)*vy/vx)+y)
