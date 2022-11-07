6bcc10bb807977a2c3fb3f45a4f8b991fee8b2ea
kawano <hi18014@student.miyazaki-u.ac.jp>
2022-11-07 17:23:03 +0900

    update:型の指定を変更

--- rear_camera_py/rear_camera/black_extractor.py
+++ rear_camera_py/rear_camera/black_extractor.py
@@ -4,7 +4,6 @@
 @author kodama0720
 """
 import cv2
-from typing import List
 import numpy as np
 
 
@@ -12,14 +11,14 @@ class BlackExtractor:
     """画像の色変換クラス."""
 
     @staticmethod
-    def extract_black(rear_camera_img: cv2.Mat) -> List[int]:
+    def extract_black(rear_camera_img: np.ndarray) -> np.ndarray:
         """画像から黒線を抽出する関数.
 
         Args:
             rear_camera_img (cv2.Mat): リアカメラ画像
 
         Returns:
-            2値変換した画像データ: List[int]
+            np.ndarray: 2値変換した画像データ
         """
         lower = np.array([0, 0, 80])  # 白と識別するための最低HSV値
         upper = np.array([180, 255, 255])  # 白と識別するための最高HSV値
