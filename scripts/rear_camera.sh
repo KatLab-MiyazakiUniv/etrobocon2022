9b06cfe4bd31834b6237644b3f0591f05fc99c95
KatLab-MiyazakiUniv <noreply@earth.cs.miyazaki-u.ac.jp>
2022-11-08 21:04:28 +0900

    update: キャリブレーションに対応

--- scripts/rear_camera.sh
+++ scripts/rear_camera.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# キャリブレーションの実行
+if [[ "${1}" == "--calibrate" ]]; then
+    bash -c "echo calibrate | nc 127.0.0.1 10338"
+    echo ''  # 改行をする
+    exit 0
+fi
+
+# 角度補正の場合
 port=${1}
 if [[ "${1}" == "" ]]; then
     # デフォルト値
