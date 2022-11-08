a7a4fc298064d16877e234c85f65359e80cd8c83
KatLab-MiyazakiUniv <noreply@earth.cs.miyazaki-u.ac.jp>
2022-11-08 21:01:08 +0900

    update: サーバ用のコールバック関数と、デバッグ用処理の追加

--- rear_camera_py/rear_camera/calibrator.py
+++ rear_camera_py/rear_camera/calibrator.py
@@ -3,7 +3,9 @@
 @author Takahiro55555
 """
 
+import datetime
 import json
+import os
 from typing import Tuple, List
 
 import cv2
@@ -19,7 +21,9 @@ class Calibrator:
     def __init__(
         self, camera_interface: CameraInterface,
         trans_mat_file: str = "rear_camera_param.npy",
-        distance_file: str = "rear_camera_distance_param.json"
+        distance_file: str = "rear_camera_distance_param.json",
+        debug: bool = False,
+        debug_dir="debug"
     ) -> None:
         """コンストラクタ.
 
@@ -29,12 +33,21 @@ class Calibrator:
             trans_mat_file (str, optional): 射影変換用パラメータファイル名. Defaults to "rear_camera_param.npy".
             distance_file (str, optional): 射影変換後の画像座標と走行体の中心からの距離等の関係を保持するパラメータファイル名.
                                            Defaults to "rear_camera_distance_param.json".
+            debug (bool, optional): Trueに設定するとデバッグ用の画像ファイルが生成される. Defaults to False.
+            debug_dir (str, optional): デバッグ用の画像ファイルを生成するディレクトリ(存在しない場合は自動で生成される).
+                                       Defaults to "debug".
         """
         self.__camera_interface = camera_interface
         self.__aruco_dictionary = aruco.getPredefinedDictionary(
             aruco.DICT_4X4_50)
         self.__trans_mat_file = trans_mat_file
         self.__distance_file = distance_file
+        self.__debug = debug
+        if not os.path.exists(debug_dir):
+            os.makedirs(debug_dir)
+        self.__debug_dir = debug_dir
+        now = datetime.datetime.now()
+        self.__debug_time = now.strftime("%Y-%m-%d_%H-%M-%S.%f")
 
     def _calc_param(self, img: np.ndarray) -> Tuple[np.ndarray, float, float]:
         """各種パラメータを計算する関数.
@@ -88,17 +101,19 @@ class Calibrator:
             frame_coordinates, target_coordinates)
         return trans_mat, distance_from_center_52_5mm, height_offset_from_center
 
-    def calibrate(self):
+    def calibrate(self) -> None:
         """キャリブレーションを行い、結果をパラメータファイルとして出力する関数.
 
         Raises:
-            RuntimeError: リアカメラの画像を取得できなかった際に発生する.
+            RuntimeError: 画像を取得できない際に発生する
         """
+        now = datetime.datetime.now()
+        self.__debug_time = now.strftime("%Y-%m-%d_%H-%M-%S.%f")
         img = self.__camera_interface.capture_image()
         if img is None:
             raise RuntimeError("Could not capture camera image.")
-        trans_mat, distance_from_center_52_5mm, height_offset_from_center = self._calc_param(
-            img)
+        self.__save_debug_img(img, 'calibration', 'captured.png')
+        trans_mat, distance_from_center_52_5mm, height_offset_from_center = self._calc_param(img)
         np.save(self.__trans_mat_file, trans_mat)
         distance_data = {
             "distance_from_center_52_5mm": distance_from_center_52_5mm,
@@ -106,6 +121,21 @@ class Calibrator:
         }
         with open(self.__distance_file, mode="w") as fp:
             json.dump(distance_data, fp)
+
+    def calibration_handler(self, cmd: str) -> str:
+        """calibrate関数のラッパ(サーバに渡すコールバック関数として利用).
+
+        Args:
+            cmd (str): サーバがこの関数を呼び出す際に渡すデータ
+
+        Returns:
+            str: キャリブレーション結果に関するメッセージ
+        """
+        try:
+            self.calibrate()
+        except Exception as e:
+            return "[ERROR] %s" % e
+        return "[Info] Successfully calibrated"
 
     def __get_marker_mean(
         self,
@@ -131,4 +161,19 @@ class Calibrator:
             if (id[0] == target_id):
                 v = np.mean(corners[i][0], axis=0)  # マーカーの四隅の座標から中心の座標を取得する
                 return v[0], v[1]
-        raise ValueError("Index(%d) not found")
+        raise ValueError("Index(%d) not found" % target_id)
+
+    def __save_debug_img(self, img: np.ndarray, prefix: str, suffix: str) -> None:
+        """デバッグ用に画像をファイルに保存する関数.
+
+        デバッグモードがFalseの場合は何も実行しない.
+
+        Args:
+            img (np.ndarray): 保存する画像データ
+            prefix (str): ファイル名の先頭文字列
+            suffix (str): 拡張子等を含めたファイル名の末尾
+        """
+        if self.__debug:
+            img_fname = "%s_%s_%s" % (prefix, self.__debug_time, suffix)
+            debug_img_path = os.path.join(self.__debug_dir, img_fname)
+            cv2.imwrite(debug_img_path, img)
