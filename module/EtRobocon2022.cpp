533041661048e374dbe5b028a6fa7e6005284e7e
Takahiro55555 <31930148+Takahiro55555@users.noreply.github.com>
2022-11-09 04:41:20 +0900

    reset: 実験用コードを実験前のコードに戻した

--- module/EtRobocon2022.cpp
+++ module/EtRobocon2022.cpp
@@ -8,7 +8,6 @@
 #include "LineTraceArea.h"
 #include "GameArea.h"
 #include "Calibrator.h"
-#include "SystemInfo.h"
 
 void EtRobocon2022::start()
 {
@@ -29,24 +28,26 @@ void EtRobocon2022::start()
 
   // キャリブレーションする
   calibrator.run();
+  isLeftCourse = calibrator.getIsLeftCourse();
+  isLeftEdge = isLeftCourse;
+  targetBrightness = calibrator.getTargetBrightness();
 
   // 合図を送るまで待機する
   calibrator.waitForStart();
 
-  for(int i = 0; i < 8; i++) {
-    // 回頭する
-    Rotation rt(45, 70, true);
-    rt.run();
+  // カメラシステムに開始合図を送る
+  system("bash ./etrobocon2022/scripts/start.sh");
 
-    // 回頭補正を行う
-    if(i % 2 == 0) {
-      CorrectingRotation cr(0, 65);
-      cr.run();
-    } else {
-      CorrectingRotation cr(45, 65);
-      cr.run();
-    }
-  }
+  // スタートのメッセージログを出す
+  const char* course = isLeftCourse ? "Left" : "Right";
+  snprintf(buf, BUF_SIZE, "\nRun on the %s Course\n", course);
+  logger.logHighlight(buf);
+
+  // ライントレースエリアを走行する
+  LineTraceArea::runLineTraceArea(isLeftCourse, isLeftEdge, targetBrightness);
+
+  // ゲームエリアを攻略する
+  GameArea::runGameArea(isLeftCourse, isLeftEdge, targetBrightness);
 
   // 走行終了のメッセージログを出す
   logger.logHighlight("The run has been completed\n");
@@ -60,6 +61,5 @@ void EtRobocon2022::sigint(int _)
   Logger logger;
   logger.log("Forced termination.");  // 強制終了のログを出力
   logger.outputToFile();              // ログファイルを生成
-
-  _exit(0);  //システムコールで強制終了
+  _exit(0);                           //システムコールで強制終了
 }
