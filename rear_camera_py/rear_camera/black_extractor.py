12304680081f644be0b81d87f13c15dcbcf03fe9
miyashita64 <javajavawookjavawook@gmail.com>
2022-11-09 20:10:51 +0900

    update: 黒線抽出の閾値を更新

--- rear_camera_py/rear_camera/black_extractor.py
+++ rear_camera_py/rear_camera/black_extractor.py
@@ -20,12 +20,28 @@ class BlackExtractor:
         Returns:
             np.ndarray: 2値変換した画像データ
         """
-        lower = np.array([0, 0, 80])  # 白と識別するための最低HSV値
-        upper = np.array([180, 255, 255])  # 白と識別するための最高HSV値
+        black_lower = np.array([0, 0, 0])  # 黒と識別するための最低HSV値
+        black_upper = np.array([180, 100, 60])  # 黒と識別するための最高HSV値
 
         # BGR色空間からHSV色空間への変換
         hsv = cv2.cvtColor(rear_camera_img, cv2.COLOR_BGR2HSV)
         # 2値変換
-        binary_img = cv2.inRange(hsv, lower, upper)
-
+        binary_img = 255 - cv2.inRange(hsv, black_lower, black_upper)
+        # 以下、テスト用
+        binary_img = np.array([binary_img.T[:,:],binary_img.T[:,:],binary_img.T[:,:]]).T
+        test_img = im_v = cv2.vconcat([binary_img, img])
+        return test_img
+        # 以上、テスト用
         return binary_img
+
+for i in range(1,21):
+    filename = "course_img (%d).png" % i
+    img = cv2.imread("test_image/%s" % filename)
+    binary_img = BlackExtractor.extract_black(img)
+    cv2.imwrite("result_image/%s" % filename, binary_img)
+
+for i in range(1,34):
+    filename = "trans_image (%d).png" % i
+    img = cv2.imread("test_image/%s" % filename)
+    binary_img = BlackExtractor.extract_black(img)
+    cv2.imwrite("result_image/%s" % filename, binary_img)
