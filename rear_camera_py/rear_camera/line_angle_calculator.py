f60be19ba15f7922257077b18700b3057a7bef07
kawano <hi18014@student.miyazaki-u.ac.jp>
2022-11-11 03:37:15 +0900

    defacter: 変数名の変更

--- rear_camera_py/rear_camera/line_angle_calculator.py
+++ rear_camera_py/rear_camera/line_angle_calculator.py
@@ -3,9 +3,10 @@
 @author: Takahiro55555 kawanoichi
 """
 
+import json
 import os
 import time
-from typing import Union
+from typing import Tuple, Union
 import cv2
 import numpy as np
 
@@ -20,6 +21,7 @@ class LineAngleCalculator:
         self,
         camera_interface: CameraInterface,
         trans_mat_file: str = "rear_camera_param.npy",
+        distance_file: str = "rear_camera_distance_param.json",
         debug: bool = False,
         debug_dir="debug"
     ) -> None:
@@ -40,6 +42,8 @@ class LineAngleCalculator:
         self.__camera_interface = camera_interface
         if not os.path.isfile(trans_mat_file):
             raise FileNotFoundError("file name: '%s'" % trans_mat_file)
+        if not os.path.isfile(distance_file):
+            raise FileNotFoundError("file name: '%s'" % distance_file)
         self.__trans_mat = np.load(trans_mat_file)
 
         # NOTE: 射影変換後の画像において、4つのArUcoマーカの中心点と各辺の中点が一致する正方形を考える.
@@ -53,6 +57,23 @@ class LineAngleCalculator:
         #  ただし、射影変換後の画像下部より更に下の部分に走行体の原点が存在することに留意すること.
         #  また、現実の座標系と画像の座標の差異にも留意すること.
 
+        #  jsonファイルの読み込み
+        with open(distance_file) as fp:
+            distance_data = json.load(fp)
+       
+        key = "distance_from_center_52_5mm"
+        if key not in distance_data:
+            raise KeyError("key not found: '%s', file: %s" %
+                           (key, distance_file))
+        self.__distance_from_center_52_5mm = distance_data[key]
+        
+        key = "height_offset_from_center"
+        if key not in distance_data:
+            raise KeyError("key not found: '%s', file: %s" %
+                           (key, distance_file))
+        self.__height_offset_from_center = distance_data[key]
+        
+        # デバックファイルがない場合は作成を行う
         self.__debug = debug
         if not os.path.exists(debug_dir):
             os.makedirs(debug_dir)
@@ -91,14 +112,14 @@ class LineAngleCalculator:
         binary_img = BlackExtractor.extract_black(img_transformed)
 
         # 線分を検出(線分上の2点座標[x1,y1,x2,y2])
-        detect_line = self.detect_line_segment(binary_img)
+        detected_line = self.detect_line_segment(binary_img)
 
-        if detect_line is None:
+        if detected_line is None:
             return None
 
         # 単位ベクトルに変換[x1-x2, y1-y2]
-        a = np.array([detect_line[0]-detect_line[2],
-                      detect_line[1] - detect_line[3]])
+        a = np.array([detected_line[0]-detected_line[2],
+                      detected_line[1] - detected_line[3]])
         x = np.linalg.norm(a)
         your_line = a / x
 
@@ -117,7 +138,7 @@ class LineAngleCalculator:
         if self.__debug:
             # 各画像の保存
             debug_img = self.draw_detected_lines_on_the_image(
-                img_transformed, detect_line, angle_2_y)
+                img_transformed, detected_line, angle_2_y)
             img_fname = "%s_%f_detected.png" % (
                 debug_img_fname_prefix, debug_time)
             debug_img_path = os.path.join(self.__debug_dir, img_fname)
@@ -141,15 +162,15 @@ class LineAngleCalculator:
     def draw_detected_lines_on_the_image(
         self,
         img: np.ndarray,
-        detect_line: np.ndarray,
+        detected_line: np.ndarray,
         angle: float
     ) -> np.ndarray:
         """検出した線分及び、その線分と機体の中心線のなす角を線分の検出用いた画像へ描画するデバッグ用関数.
-        np.array([detect_line[0]-detect_line[2], detect_line[1]-detect_line[3]])
+        np.array([detected_line[0]-detected_line[2], detected_line[1]-detected_line[3]])
 
         Args:
             img (np.ndarray): 線分の検出に用いた画像データ.
-            detect_line (np.ndarray): 描画する線分の座標 [x1,y1,x2,y2]
+            detected_line (np.ndarray): 描画する線分の座標 [x1,y1,x2,y2]
             angle (float): 検出した線分と機体の中心線とのなす角.
 
         Returns:
@@ -157,13 +178,13 @@ class LineAngleCalculator:
         """
         # 検出した線分を描画
         cv2.line(img,
-                 (int(detect_line[0]), int(detect_line[1])),
-                 (int(detect_line[2]), int(detect_line[3])),
+                 (int(detected_line[0]), int(detected_line[1])),
+                 (int(detected_line[2]), int(detected_line[3])),
                  (0, 0, 255),  # BGR
                  thickness=4)  # 描画する線分の太さ
 
         # 線分の角度を描画
-        tx, ty = int((detect_line[0]+detect_line[2])/2), int((detect_line[1]+detect_line[3])/2)
+        tx, ty = int((detected_line[0]+detected_line[2])/2), int((detected_line[1]+detected_line[3])/2)
         cv2.putText(img, "%.2f" % angle, (tx, ty),
                     cv2.FONT_HERSHEY_COMPLEX, 1.5, (0, 255, 0), thickness=2)
         return img
@@ -211,7 +232,11 @@ class LineAngleCalculator:
         lines = fld.detect(img)
 
         # 画像上側7.5割の領域上にある線、もしくは、またがってる線分を削除
-        ignore_border = int(img.shape[0] * 0.75)
+        # ignore_border = int(img.shape[0] * 0.75)
+        # lines = lines[np.where((lines[:, :, 1] > ignore_border) & (lines[:, :, 3] > ignore_border))]
+
+        img_h, img_w= img.shape[:2]
+        _, ignore_border = self.runner_base_coordinate_to_image_base_coordinate_pix(0, 600, img_h, img_w)
         lines = lines[np.where((lines[:, :, 1] > ignore_border) & (lines[:, :, 3] > ignore_border))]
 
         if len(lines) == 0:
@@ -227,18 +252,18 @@ class LineAngleCalculator:
         delete_list = []
         for l in range(len(lines)):
             # 枠(直線)との距離が dis_from_edge_threshold より小さい座標を選別する
-            if LineAngleCalculator.cal_dis(straight_line_right, lines[l, 0], lines[l, 1]) < dis_from_edge_threshold\
-                    and LineAngleCalculator.cal_dis(straight_line_right, lines[l, 2], lines[l, 3]) < dis_from_edge_threshold:
+            if LineAngleCalculator.calc_distance(straight_line_right, lines[l, 0], lines[l, 1]) < dis_from_edge_threshold\
+                    and LineAngleCalculator.calc_distance(straight_line_right, lines[l, 2], lines[l, 3]) < dis_from_edge_threshold:
                 delete_list.append(l)
                 continue
 
-            elif LineAngleCalculator.cal_dis(straight_line_left, lines[l, 0], lines[l, 1]) < dis_from_edge_threshold\
-                    and LineAngleCalculator.cal_dis(straight_line_left, lines[l, 2], lines[l, 3]) < dis_from_edge_threshold:
+            elif LineAngleCalculator.calc_distance(straight_line_left, lines[l, 0], lines[l, 1]) < dis_from_edge_threshold\
+                    and LineAngleCalculator.calc_distance(straight_line_left, lines[l, 2], lines[l, 3]) < dis_from_edge_threshold:
                 delete_list.append(l)
                 continue
 
-            elif LineAngleCalculator.cal_dis(straight_line_lower, lines[l, 0], lines[l, 1]) < dis_from_edge_threshold\
-                    and LineAngleCalculator.cal_dis(straight_line_lower, lines[l, 2], lines[l, 3]) < dis_from_edge_threshold:
+            elif LineAngleCalculator.calc_distance(straight_line_lower, lines[l, 0], lines[l, 1]) < dis_from_edge_threshold\
+                    and LineAngleCalculator.calc_distance(straight_line_lower, lines[l, 2], lines[l, 3]) < dis_from_edge_threshold:
                 delete_list.append(l)
 
         if len(delete_list) != 0:
@@ -252,7 +277,7 @@ class LineAngleCalculator:
         return lines[np.unravel_index(np.argmax(cal_lines), lines.shape)[0]]
 
     @staticmethod
-    def cal_dis(straight_line: np.ndarray, x: float, y: float) -> float:
+    def calc_distance(straight_line: np.ndarray, x: float, y: float) -> float:
         """直線と座標の距離を計算する関数.
         Args:
             straight_line (float): 直線上の2点座標を格納した配列[x1, y1, x2, y2]
@@ -264,3 +289,33 @@ class LineAngleCalculator:
         u = np.array([straight_line[2] - straight_line[0], straight_line[3] - straight_line[1]])
         v = np.array([x - straight_line[0], y - straight_line[1]])
         return abs(np.cross(u, v) / np.linalg.norm(u))
+
+    def runner_base_coordinate_to_image_base_coordinate_pix(
+        self,
+        rx: Union[int, float],
+        ry: Union[int, float],
+        img_h: int,
+        img_w: int
+    ) -> Tuple[int, int]:
+        """機体の中心を原点とした座標に変換する.
+        Args:
+            rx (Union[int, float]): 走行体の中心を原点としたX座標[mm].
+            ry (Union[int, float]): 走行体の中心を原点としたY座標[mm].
+            img_h (int): 画像の縦幅[pix].
+            img_w (int): 画像の横幅[pix].
+        Returns:
+            Tuple[int, int]: 変換後の座標(x, y)[pix].
+        """
+        pix_x = self.mm_to_pix(rx) + img_w/2
+        pix_y = self.__height_offset_from_center + 312.5 - self.mm_to_pix(ry)
+        return pix_x, pix_y
+
+    def mm_to_pix(self, mm: Union[int, float]) -> float:
+        """mmをpixに変換する.
+        Args:
+            mm (Union[int, float]): 変換したい値[mm]
+        Returns:
+            float: 変換結果[mm]
+        """
+        return mm * self.__distance_from_center_52_5mm / 52.5
+    
