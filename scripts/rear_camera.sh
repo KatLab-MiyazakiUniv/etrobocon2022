9b753cad17c81c939b615810b7ab0b3882c2615a
Takahiro55555 <31930148+Takahiro55555@users.noreply.github.com>
2022-11-10 23:43:25 +0900

    refactor: ポート番号のハードコードを修正

--- scripts/rear_camera.sh
+++ scripts/rear_camera.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+# デフォルト値
+port=10338
+
 # キャリブレーションの実行
 if [[ "${1}" == "--calibrate" ]]; then
-    bash -c "echo calibrate | nc 127.0.0.1 10338"
+    bash -c "echo calibrate | nc 127.0.0.1 ${port}"
     echo ''  # 改行をする
     exit 0
 fi
 
 # 角度補正の場合
-port=${1}
-if [[ "${1}" == "" ]]; then
-    # デフォルト値
-    port=10338
+if [[ "${1}" != "" ]]; then
+    port=${1}
 fi
 
 bash -c "echo angle | nc 127.0.0.1 ${port}"
